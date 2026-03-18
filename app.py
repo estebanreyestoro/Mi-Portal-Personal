2c6b81eaeb9d12c14ac110619da149d8844ebf58
estebanreyestoro <esteban.sr828@gmail.com>
2026-03-17 23:42:16 -0300

    Corrigiendo nombre de variable y agregando headers

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def obtener_hora_y_fecha(fecha_str):
 def procesar_google_news(url):
     noticias = []
     try:
-        headers = {"User-Agent": "Mozilla/5.0"}
-        res = requests.get(url, headers=headers, timeout=10)
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
+        res = requests.get(url, headers=headers)
         sopa = BeautifulSoup(res.text, "xml")
         for item in sopa.find_all('item', limit=8):
             fecha_dt = obtener_hora_y_fecha(item.pubDate.text if item.find('pubDate') else "")
